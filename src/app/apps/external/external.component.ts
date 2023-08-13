400225b5badf1910a8d9ab595844af28ec21747c
alexmelk <alexmelk@yandex.ru>
2023-08-13 16:18:44 +0300

    Task #6. Универсальное окно приложений. Добавлены названия и иконки

--- src/app/apps/external/external.component.ts
+++ src/app/apps/external/external.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { ExternalAppName } from './enum/external-app-title';
 import { EXTERNAL_APPS, ExternalApp } from './const/external-apps';
 
@@ -7,17 +7,22 @@ import { EXTERNAL_APPS, ExternalApp } from './const/external-apps';
   templateUrl: './external.component.html',
   styleUrls: ['./external.component.less'],
 })
-export class ExternalComponent implements OnInit {
+export class ExternalComponent implements OnInit, OnDestroy {
   @Input() externalAppName: ExternalAppName | null = null;
 
   appLink: string = '';
+  externalAppInfo: ExternalApp | null | undefined = null;
 
   constructor() {}
 
   ngOnInit(): void {
     if (this.externalAppName) {
-      this.appLink =
-        EXTERNAL_APPS.find((x) => x.name === this.externalAppName)?.url || '';
+      this.externalAppInfo = EXTERNAL_APPS.find(
+        (x) => x.name === this.externalAppName
+      );
+      this.appLink = this.externalAppInfo?.url || '';
     }
   }
+
+  ngOnDestroy(): void {}
 }
